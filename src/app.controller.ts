e07aa0abd7035303be5579904bf57adb08ab9f1d
seirra blake <sophietheopossum@yandex.ru>
2021-08-25 15:37:07 +0100

    implement required api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('post/:blogName')
+  getBlogName(@Param() blogName): string {
+    const aaaaa = 'heogaeg';
+    return aaaaa;
   }
 }
